39dbd509c49757425228dcbf1fa8408add646e63
artdian <46709199+jartdian@users.noreply.github.com>
2023-02-05 22:40:35 +0100

    add styling, refactor javascript

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-const restartButton = document.querySelector(".restartGame");
+const restartButton = document.querySelector(".restartGameButton");
 const buttons = [...document.querySelectorAll("#choiceBtn")];
 const roundResult = document.querySelector(".roundResult");
 const finalResult = document.querySelector(".finalResult");
 const roundsInput = document.querySelector(".rounds");
+const playerChoiceText = document.querySelector(".playerChoiceText");
+const computerChoiceText = document.querySelector(".computerChoiceText");
+const playerEmoji = document.querySelector('.playerEmoji')
 
 let round = 0;
 let score = { win: 0, lose: 0 };
 
 buttons.forEach((button) => button.addEventListener("click", playRound));
 restartButton.addEventListener("click", restartGame);
+
+function getRandomUnhappyEmoji() {
+  let randEmoji = Math.floor(Math.random() * 3);
+  return randEmoji === 0 ? "😒" : randEmoji === 1 ? "😔" : "😭";
+}
 
 function getComputerChoice() {
   let rand = Math.floor(Math.random() * 3);
@@ -40,21 +48,32 @@ function versus(playerSelection, computerSelection) {
   }
 }
 
-
 function restartGame() {
   roundResult.textContent = "";
   finalResult.textContent = "";
+  playerChoiceText.textContent = "";
+  computerChoiceText.textContent = "";
+  playerEmoji.textContent = "🙂";
+  finalResult.classList.remove("winnerText");
+  finalResult.classList.remove("loserText");
+
   score = { win: 0, lose: 0 };
   round = 0;
   roundsInput.disabled = false;
+  buttons.forEach((button) => (button.disabled = false));
 }
 
 function result() {
   if (score.win > score.lose) {
-    finalResult.textContent = `Congratulations You Win \n \t ${score.win}-${score.lose}`;
+    finalResult.classList.add("winnerText");
+    finalResult.textContent = `Congratulations You Won 🥳 
+    ${score.win}-${score.lose}`;
   }
   if (score.win < score.lose) {
-    finalResult.textContent = `Unfortunately You Lost \n \t ${score.win}-${score.lose}`;
+    finalResult.classList.add("loserText");
+    finalResult.textContent = `Unfortunately You Lost 🥺 
+    ${score.win}-${score.lose}`;
+    playerEmoji.textContent = getRandomUnhappyEmoji()
   } else if (score.win === score.lose) {
     finalResult.textContent = `It's a Draw! \n ${score.win}-${score.lose}`;
   }
@@ -67,10 +86,17 @@ function playRound(e) {
   let playerSelection =
     e.target.className[0].toUpperCase() + e.target.className.slice(1);
   let computerSelection = getComputerChoice();
-  round++
+  playerChoiceText.textContent = playerSelection;
+  computerChoiceText.textContent = computerSelection;
+  round++;
+  
   if (round === rounds) {
-    versus(playerSelection, computerSelection)
+    versus(playerSelection, computerSelection);
+    buttons.forEach((button) => (button.disabled = true));
     return result();
   }
   return versus(playerSelection, computerSelection);
 }
+
+// TODO: Create a header for Current Score
+// Add media queries
